1daa57d038f77ce87961c275a6f245ccfdd027bd
GuisselC <xennia.guissel@gmail.com>
2025-04-13 22:34:00 -0500

    Actualizando archivo

--- testConnection.js
+++ testConnection.js
@@ -1,9 +1,2 @@
 console.log("¡Practica Xennia!");
 
-let num1 = 5;
-let num2 = 10;
-let suma = num1 + num2;
-
-console.log("La suma de " + num1 + " y " + num2 + " es: " + suma);
-
-console.log("Perfect")
